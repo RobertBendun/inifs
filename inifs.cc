10e1566c2ac6937a305c18c94007de140586ca98
Robert Bendun <robert@bendun.cc>
2021-11-01 18:45:25 +0100

    Value reading and key opening implemented
    Not fully, sectionless entries are still not openable. Also, only read
    is supported.

--- inifs.cc
+++ inifs.cc
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <cstddef>
 #include <cstring>
+#include <string_view>
+#include <cassert>
 
 #define FUSE_USE_VERSION 31
 #include <fuse.h>
 
 #include "ini.cc"
+
+using namespace std::string_view_literals;
 
 static struct options {
 	char const* ini_filename;
@@ -25,6 +29,18 @@ static const struct fuse_opt option_spec[] = {
 #define Content "hello, world!\n"
 
 INI ini;
+struct stat ini_stat;
+
+inline void update_stat(struct stat *dst)
+{
+
+	dst->st_ctime = ini_stat.st_ctime;
+	dst->st_atime = ini_stat.st_atime;
+	dst->st_mtime = ini_stat.st_mtime;
+
+	dst->st_uid = ini_stat.st_uid;
+	dst->st_gid = ini_stat.st_gid;
+}
 
 namespace inifs
 {
@@ -44,14 +60,30 @@ namespace inifs
 			stbuf->st_nlink = 2;
 			res = 0;
 		} else {
-			ini.for_all_sections([&](std::string const& section) {
-				if (section == path + 1) {
-					stbuf->st_mode = S_IFDIR | 0755;
-					stbuf->st_nlink = 2;
-					stbuf->st_size = 0; // TODO add size
-					res = 0;
-				}
-			});
+			std::string_view p = path+1;
+			auto split = p.find('/');
+			if (split == std::string_view::npos) {
+				ini.for_all_sections([&](std::string const& section) {
+					if (section == path + 1) {
+						stbuf->st_mode = S_IFDIR | 0755;
+						stbuf->st_nlink = 2;
+						stbuf->st_size = 0; // TODO add size
+						update_stat(stbuf);
+						res = 0;
+					}
+				});
+			} else {
+				auto target_key = p.substr(split + 1);
+				ini.for_section(p.substr(0, split), [&](std::string const& key, std::string const& value) {
+					if (key == target_key) {
+						stbuf->st_mode = S_IFREG | 0644;
+						stbuf->st_nlink = 1;
+						stbuf->st_size = value.size();
+						update_stat(stbuf);
+						res = 0;
+					}
+				});
+			}
 		}
 		return res;
 	}
@@ -63,45 +95,75 @@ namespace inifs
 			fuse_file_info*,
 			fuse_readdir_flags)
 	{
-		printf("readdir: %s\n", path);
-		if (strcmp(path, "/") != 0)
-			return -ENOENT;
-
 		filler(buf, ".", NULL, 0, {});
 		filler(buf, "..", NULL, 0, {});
+		if (path == "/"sv) {
+			ini.for_all_sections([&](std::string const& section) {
+				filler(buf, section.c_str(), NULL, 0, {});
+			});
+			return 0;
+		}
 
-		ini.for_all_sections([&](std::string const& section) {
-			filler(buf, section.c_str(), NULL, 0, {});
+		bool section_found = ini.for_section(path + 1, [&](std::string const& key, std::string const&) {
+			filler(buf, key.c_str(), NULL, 0, {});
 		});
 
-		return 0;
+		return section_found ? 0 : -ENOENT;
 	}
 
 	static int open(char const* path, fuse_file_info *fi)
 	{
-		if (strcmp(path+1, "xd") != 0)
-			return -ENOENT;
+		auto p = std::string_view(path + 1);
+		auto split = p.find('/');
 
-		if ((fi->flags & O_ACCMODE) != O_RDONLY)
-			return -EACCES;
+		if (split == std::string::npos) {
+			// Sectionless entries
+			assert(false && "unimplemented");
+		} else {
+			auto const target_key = p.substr(split + 1);
+			bool key_found = false;
+			ini.for_section(p.substr(0, split), [&](std::string const& key, std::string const&) {
+				if (target_key == key) {
+					key_found = true;
+				}
+			});
+
+			if (!key_found)
+				return -ENOENT;
+
+			if ((fi->flags & O_ACCMODE) != O_RDONLY)
+				return -EACCES;
+		}
 
 		return 0;
 	}
 
 	static int read(char const* path, char *buf, size_t size, off_t offset, fuse_file_info *)
 	{
-		size_t len;
+		auto p = std::string_view(path + 1);
+		auto split = p.find('/');
 
-		if (strcmp(path+1, "xd") != 0)
-			return -ENOENT;
-
-		len = strlen(Content);
-		if ((unsigned)offset < len) {
-			if (offset + size > len)
-				size = len - offset;
-			memcpy(buf, Content + offset, size);
+		if (split == std::string::npos) {
+			// Sectionless entries
+			assert(false && "unimplemented");
 		} else {
-			size = 0;
+			auto const target_key = p.substr(split + 1);
+			bool key_found = false;
+			ini.for_section(p.substr(0, split), [&](std::string const& key, std::string const& value) {
+				if (target_key == key) {
+					key_found = true;
+					if ((unsigned)offset < value.size()) {
+						if (offset + size > value.size())
+							size = value.size() - offset;
+						memcpy(buf, value.data() + offset, size);
+					} else {
+						size = 0;
+					}
+				}
+			});
+
+			if (!key_found)
+				return -ENOENT;
 		}
 
 		return size;
@@ -134,6 +196,7 @@ int main(int argc, char **argv)
 		return 3;
 	}
 
+	lstat(options.ini_filename, &ini_stat);
 
 	auto ret = fuse_main(args.argc, args.argv, &oper, nullptr);
 	fuse_opt_free_args(&args);
